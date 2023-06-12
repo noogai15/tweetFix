b83b1b828bea57647e2b3b3dc016a0aef8697e23
Yakup <ykohne@gmail.com>
2023-06-12 16:13:30 +0200

    Clean up

--- src/link.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var twitterUrls = ["http://twitter.com/", "https://twitter.com/"];
-function hasValidTwitterLink(url) {
-    return twitterUrls.some(function (twitterUrl) { return url.includes(twitterUrl) && url.includes("status"); });
-}
-exports.hasValidTwitterLink = hasValidTwitterLink;
